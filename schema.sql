9ee0fdd1d17334c9ba0987e1901c342ed56d8a9c
Frank <fcberni@hotmail.com>
2020-11-05 15:37:05 -0500

    Added new retirement table

--- schema.sql
+++ schema.sql
@@ -41,22 +41,32 @@ CREATE TABLE salaries (
 --Making a table for our dept_emp.csv
 CREATE TABLE dept_emp (
   emp_no INT NOT NULL,
-  dept_no VARCHAR NOT NULL,
+  dept_no VARCHAR(4) NOT NULL,
   from_date DATE NOT NULL,
   to_date DATE NOT NULL,
   FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
   FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
-  PRIMARY KEY (emp_no)
+  PRIMARY KEY (emp_no, dept_no)
 );
+
+Drop table titles;
 
 --Making a table for our Titles.csv
 CREATE TABLE titles (
-  emp_no INT NOT NULL,
-  title VARCHAR NOT NULL,
-  from_date DATE NOT NULL,
-  to_date DATE NOT NULL,
-  FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-  PRIMARY KEY (emp_no)
+    emp_no INT NOT NULL,
+    titles VARCHAR(50) NOT NULL,
+    from_date DATE NOT NULL,
+    to_date DATE,
+    FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+    PRIMARY KEY (emp_no, titles, from_date)
 );
 
 SELECT * FROM departments;
+SELECT * FROM employees;
+SELECT * FROM dept_manager;
+SELECT * FROM salaries;
+SELECT * FROM dept_emp;
+
+--Error importing; I troubleshooted by adding titles and from_date to the primary key. 
+--Also corrected column header title to titles to solve error  column "titles" of relation "titles" does not exist when I tried importing
+SELECT * FROM titles;
